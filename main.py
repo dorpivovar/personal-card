275112435dcca669f580ea379c21d498ccca679b
dorpivovar <armandooganesyan@gmail.com>
2024-02-14 18:46:59 +0300

    first commit

--- main.py
+++ main.py
@@ -1,36 +1,18 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-from flask import Flask, redirect, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-class LoginForm(FlaskForm):
-    userid = StringField('Id астронавта', validators=[DataRequired()])
-    password_1 = PasswordField('Пароль астронавта', validators=[DataRequired()])
-    cap_id = StringField('Id капитана', validators=[DataRequired()])
-    password_2 = PasswordField('Пароль капитана', validators=[DataRequired()])
-    submit = SubmitField('Доступ')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('login.html', title='Аварийный доступ', form=form)
+@app.route('/distribution')
+def distribution():
+    astronauts = ['Ридли Скотт', 'Энди Уир', 'Марк Уотни',
+                  'Венката Капур', 'Тедди Сандерс', 'Шон Бин']
+    return render_template('distribution.html', astronauts=astronauts)
 
 
 @app.route('/')
 def index():
-    return redirect('/login')
-
-
-@app.route('/success')
-def success():
-    return render_template('success.html')
+    return redirect('/distribution')
 
 
 if __name__ == '__main__':
