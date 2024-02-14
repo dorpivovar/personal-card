e7e6ac1c63ce2da777cc2df3a57dec310f962408
dorpivovar <armandooganesyan@gmail.com>
2024-02-14 18:42:23 +0300

    first commit

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/list_prof/<lst>')
-def index(lst):
-    professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
-                   'инженер по терраформированию', 'климатолог',
-                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
-                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
-                   'штурман', 'пилот дронов']
-    return render_template('list_prof.html', list=lst, professions=professions)
+@app.route('/answer')
+@app.route('/auto_answer')
+def auto_answer():
+    profil = {}
+    profil['title'] = 'Анкета'
+    profil['surname'] = 'Watny'
+    profil['name'] = 'Mark'
+    profil['education'] = 'выше среднего'
+    profil['profession'] = 'штурман марсохода'
+    profil['sex'] = 'male'
+    profil['motivation'] = 'Всегда мечтал застрять на Марсе!'
+    profil['ready'] = 'True'
+    return render_template('auto_answer.html', **profil)
 
 
 if __name__ == '__main__':
