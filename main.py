fa2dc4a45cc2cb5cfed1bb45b1af90643dfcdb96
dorpivovar <armandooganesyan@gmail.com>
2024-03-12 14:34:46 +0300

    first commit

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
+import json
+
 from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
-@app.route('/distribution')
-def distribution():
-    astronauts = ['Ридли Скотт', 'Энди Уир', 'Марк Уотни',
-                  'Венката Капур', 'Тедди Сандерс', 'Шон Бин']
-    return render_template('distribution.html', astronauts=astronauts)
+@app.route('/member')
+def member():
+    with open('static/members.json', 'r', encoding='utf8') as f:
+        members = json.load(f)['members']
+    return render_template('member.html', members=members)
 
 
 @app.route('/')
 def index():
-    return redirect('/distribution')
+    return redirect('/member')
 
+
+if __name__ == '__main__':
+    app.run("", 8080)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
